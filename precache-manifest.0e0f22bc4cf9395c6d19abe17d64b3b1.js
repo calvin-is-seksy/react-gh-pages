4849a9be2428163d21ef080e639b06e1fa45a690
Calvin Wang <calvin.wang0628@gmail.com>
2019-03-20 02:30:12 -0700

    Updates

---
+++ precache-manifest.0e0f22bc4cf9395c6d19abe17d64b3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03ae6ace1d41860cebcf",
+    "url": "/react-gh-pages/static/css/main.4de2247d.chunk.css"
+  },
+  {
+    "revision": "03ae6ace1d41860cebcf",
+    "url": "/react-gh-pages/static/js/main.77affcf7.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-gh-pages/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c04aebe7b748ccc10f743832468848d5",
+    "url": "/react-gh-pages/index.html"
+  }
+];
